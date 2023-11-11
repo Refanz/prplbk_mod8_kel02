d38c1122687089f8930a77e436b6c4ee562a11d0
Refanzzzz <refandasuryasaputra@gmail.com>
2023-11-11 16:38:01 +0700

    add celcius conv

--- Form1.vb
+++ Form1.vb
@@ -2,5 +2,51 @@
 
     Private objConv As New TempConv.TempConvComp
 
+    Private Sub rbCToR_CheckedChanged(sender As Object, e As EventArgs) Handles rbCToR.CheckedChanged
+        Dim c As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            c = 0
+        Else
+            c = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Celcius Ke Reamur" & vbNewLine & vbNewLine & "Celcius: " & temp & vbNewLine & "Reamur: " & objConv.cToR(c)
+
+    End Sub
+
+    Private Sub rbCToF_CheckedChanged(sender As Object, e As EventArgs) Handles rbCToF.CheckedChanged
+        Dim c As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            c = 0
+        Else
+            c = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Celcius Ke Fahrenheit" & vbNewLine & vbNewLine & "Celcius: " & temp & vbNewLine & "Fahrenheit: " & objConv.cToF(c)
+    End Sub
+
+    Private Sub rbCToK_CheckedChanged(sender As Object, e As EventArgs) Handles rbCToK.CheckedChanged
+        Dim c As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            c = 0
+        Else
+            c = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Celcius Ke Kelvin" & vbNewLine & vbNewLine & "Celcius: " & temp & vbNewLine & "Kelvin: " & objConv.cToK(c)
+    End Sub
+
 
 End Class
