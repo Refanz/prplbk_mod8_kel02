d837bbb5c785625bd1b8f4dbc727cb2d233e84a3
Refanzzzz <refandasuryasaputra@gmail.com>
2023-11-15 21:04:21 +0700

    add other convert

--- Form1.vb
+++ Form1.vb
@@ -47,6 +47,138 @@
 
         valResult.Text = "Konversi Celcius Ke Kelvin" & vbNewLine & vbNewLine & "Celcius: " & temp & vbNewLine & "Kelvin: " & objConv.cToK(c)
     End Sub
+    Private Sub rbRToC_CheckedChanged(sender As Object, e As EventArgs) Handles rbRToC.CheckedChanged
+        Dim r As Double
+        Dim temp As String
 
+        temp = valTemp.Text
 
+        If temp = "" Then
+            r = 0
+        Else
+            r = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Reamur Ke Celicus" & vbNewLine & vbNewLine & "Reamur: " & temp & vbNewLine & "Celcius: " & objConv.rToC(r)
+    End Sub
+
+    Private Sub rbRToF_CheckedChanged(sender As Object, e As EventArgs) Handles rbRToF.CheckedChanged
+        Dim r As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            r = 0
+        Else
+            r = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Reamur Ke Fahrenheit" & vbNewLine & vbNewLine & "Reamur: " & temp & vbNewLine & "Fahrenheit: " & objConv.rToF(r)
+    End Sub
+
+    Private Sub rbRToK_CheckedChanged(sender As Object, e As EventArgs) Handles rbRToK.CheckedChanged
+        Dim r As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            r = 0
+        Else
+            r = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Reamur Ke Kelvin" & vbNewLine & vbNewLine & "Reamur: " & temp & vbNewLine & "Kelvin: " & objConv.rToK(r)
+    End Sub
+
+    Private Sub rbFToC_CheckedChanged(sender As Object, e As EventArgs) Handles rbFToC.CheckedChanged
+        Dim f As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            f = 0
+        Else
+            f = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Fahrenheit Ke Celicus" & vbNewLine & vbNewLine & "Fahrenheit: " & temp & vbNewLine & "Celcius: " & objConv.fToC(f)
+    End Sub
+
+    Private Sub rbFToR_CheckedChanged(sender As Object, e As EventArgs) Handles rbFToR.CheckedChanged
+        Dim f As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            f = 0
+        Else
+            f = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Fahrenheit Ke Reamur" & vbNewLine & vbNewLine & "Fahrenheit: " & temp & vbNewLine & "Reamur: " & objConv.fToR(f)
+    End Sub
+
+    Private Sub rbFToK_CheckedChanged(sender As Object, e As EventArgs) Handles rbFToK.CheckedChanged
+        Dim f As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            f = 0
+        Else
+            f = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Fahrenheit Ke Kelvin" & vbNewLine & vbNewLine & "Fahrenheit: " & temp & vbNewLine & "Kelvin: " & objConv.fToK(f)
+    End Sub
+
+    Private Sub rbKToC_CheckedChanged(sender As Object, e As EventArgs) Handles rbKToC.CheckedChanged
+        Dim k As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            k = 0
+        Else
+            k = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Kelvin Ke Celicus" & vbNewLine & vbNewLine & "Kelvin: " & temp & vbNewLine & "Celcius: " & objConv.kToC(k)
+    End Sub
+
+    Private Sub rbKToR_CheckedChanged(sender As Object, e As EventArgs) Handles rbKToR.CheckedChanged
+        Dim k As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            k = 0
+        Else
+            k = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Kelvin Ke Reamur" & vbNewLine & vbNewLine & "Kelvin: " & temp & vbNewLine & "Reamur: " & objConv.kToR(k)
+    End Sub
+
+    Private Sub rbKToF_CheckedChanged(sender As Object, e As EventArgs) Handles rbKToF.CheckedChanged
+        Dim k As Double
+        Dim temp As String
+
+        temp = valTemp.Text
+
+        If temp = "" Then
+            k = 0
+        Else
+            k = CDbl(valTemp.Text)
+        End If
+
+        valResult.Text = "Konversi Kelvin Ke Fahrenheit" & vbNewLine & vbNewLine & "Kelvin: " & temp & vbNewLine & "Fahrenheit: " & objConv.kToF(k)
+    End Sub
 End Class
